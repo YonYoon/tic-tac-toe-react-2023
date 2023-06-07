d20283a7c3166bd669aeb62fa1ed86ef8a444490
YonYoon <zhzhzh1806@gmail.com>
2023-06-07 15:23:20 +0600

    finally

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,33 @@ import MainLayout from "./layouts/MainLayout";
 import Zagruzka from "./components/Zagruzka";
 import Grid from "./components/Grid";
 
+function checkWinner(grid) {
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+for (let combo of winningCombos) {
+    const [a, b, c] = combo;
+
+    if (
+      grid[a].text &&
+      grid[a].text === grid[b].text &&
+      grid[b].text === grid[c].text
+    ) {
+      return grid[a].text; 
+    }
+  }
+
+  return null;
+}
+
 function App() {
   const [grid, setGrid] = useState([
     { id: 1, text: "" },
@@ -16,10 +43,13 @@ function App() {
     { id: 9, text: "" },
   ]);
   const [user, setUser] = useState(true);
+  const [winner, setWinner] = useState(null);
 
   function handlePlay(elementID) {
     //NO NO push splice pop shift unshift
     //OK map forEach slice  filter find some
+    if (winner) return;
+
     const newGrid = grid.map((item) => {
       if (item.id === elementID && !item.text) {
         return { ...item, text: user ? "X" : "O" };
@@ -27,6 +57,11 @@ function App() {
     });
     setUser(!user);
     setGrid(newGrid);
+
+    const winnerText = checkWinner(newGrid);
+    if (winnerText) {
+      setWinner(winnerText);
+    }
   }
 
   // if (!user) {
@@ -39,7 +74,11 @@ function App() {
 
   return (
     <MainLayout>
-      <Grid grid={grid} handlePlay={handlePlay} />
+      {winner ? (
+        <div>Winner: {winner}</div>
+      ) : (
+        <Grid grid={grid} handlePlay={handlePlay} />
+      )}
     </MainLayout>
   );
 }
